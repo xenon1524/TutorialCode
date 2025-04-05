d17534c6fcb3640923235d86e4fa8b4882479821
Gael Lopez Ibañez <133434300+xenon1524@users.noreply.github.com>
2025-04-05 02:13:25 -0600

    Delete app directory

--- app/routes/destroy-contact.tsx
+++
@@ -1,9 +0,0 @@
-import { redirect } from "react-router";
-import type { Route } from "./+types/destroy-contact";
-
-import { deleteContact } from "../data";
-
-export async function action({ params }: Route.ActionArgs) {
-  await deleteContact(params.contactId);
-  return redirect("/");
-}
